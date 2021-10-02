e97bf9f3d096fbf43e76a77b3fc4cdbfe230d1b1
毛 <1296193245@qq.com>
2021-10-02 14:06:15 +0800

    添加项目文件。

---
+++ main.cpp
@@ -0,0 +1,471 @@
+﻿#include<iostream>
+#include<stdio.h>
+#include<string.h>
+#include <Windows.h>
+#include<time.h>
+#include <graphics.h>
+#include <conio.h>
+#include<algorithm>
+#include<stdlib.h>
+#include <random>
+#include <string>
+#include <fstream>
+#include <ddraw.h>
+#include<iomanip>
+#include <Psapi.h>
+#include<vector>
+#include<io.h>
+#include <mmdeviceapi.h> 
+#include <endpointvolume.h>
+#include <audioclient.h>
+#include <sapi.h>                                             // 语音头文件
+#include <sphelper.h>                                     // 语音识别头文件
+#include <thread>                                           //多线程
+#include"resource1.h"
+                                                                        //vs2019自定义控制台代码模板 by mao
+                                                                        //作者QQ1296193245
+using namespace std;
+#pragma warning(disable : 4996)
+#pragma comment (lib,"ddraw.lib")
+#pragma comment(lib,"sapi.lib")
+#pragma comment(lib,"ole32.lib")
+#pragma comment(lib,"Winmm.lib")                     // 引用 Windows Multimedia API
+#pragma intrinsic(__rdtsc)
+unsigned __int64 t1, t2, t3, t4;
+double run_time;                                                   //微秒级算法时间计时器
+LARGE_INTEGER time_start;	                                //开始时间
+LARGE_INTEGER time_over;	                                //结束时间
+double dqFreq;		                                               //计时器频率
+LARGE_INTEGER f;	                                               //计时器频率
+double tt;
+long double totaltime;
+
+inline void runstart()
+{
+	QueryPerformanceFrequency(&f);
+	dqFreq = (double)f.QuadPart;
+	t1 = __rdtsc();
+	QueryPerformanceCounter(&time_start);	       //计时开始
+}
+inline void runend()
+{
+	QueryPerformanceCounter(&time_over);	        //计时结束
+	run_time = 1000 * (time_over.QuadPart - time_start.QuadPart) / dqFreq;
+	t2 = __rdtsc();
+	t3 = t2 - t1;
+}
+void ShowProcessMemoryUsageInfo()
+{
+	HANDLE handle = GetCurrentProcess();
+	PROCESS_MEMORY_COUNTERS pmc;
+	GetProcessMemoryInfo(handle, &pmc, sizeof(pmc));
+	float memoryUsage_M = pmc.WorkingSetSize / (1024.0 * 1024.0);
+	float memoryUsage_K = pmc.WorkingSetSize / 1024.0;
+	std::cout << std::fixed << setprecision(0) << "程序内存使用率：" << memoryUsage_K << " KB = " << setprecision(2) << memoryUsage_M << " MB" << std::endl;
+}
+__int64 CompareFileTime(FILETIME time1, FILETIME time2)//转换为整数时间
+{
+	__int64 a = static_cast<long long>(time1.dwHighDateTime) << 32 | time1.dwLowDateTime;
+	__int64 b = static_cast<long long>(time2.dwHighDateTime) << 32 | time2.dwLowDateTime;
+	return   (b - a);
+}
+void rundisplay()
+{
+	cout << endl;
+	cout << "-------------------------------------------------------" << endl;
+	if (t3 < 10000)
+	{
+		cout << "CPU脉冲数：" << (t3) << endl;
+	}
+	else if (t3 < 100000000 && t3 >= 10000)
+	{
+		tt = t3;
+		cout << "CPU脉冲数：" << (tt) / 10000 << "万" << endl;
+	}
+	else if (t3 > 100000000000)
+	{
+		t4 = t3;
+		t4 = t3 / 10000000;
+		tt = t4;
+		cout << "CPU脉冲数：" << (tt) / 10 << "亿" << endl;
+	}
+	else
+	{
+		t4 = t3;
+		t4 = t3 / 10000;
+		tt = t4;
+		cout << "CPU脉冲数：" << (tt) / 10000 << "亿" << endl;
+	}
+	if (run_time > 10000)
+	{
+		cout << "算法运行时间：" << run_time / 1000 << "秒" << endl;
+	}
+	else if (run_time < 1)
+	{
+		cout << "算法运行时间：" << run_time * 1000 << "微秒" << endl;
+	}
+	else
+	{
+		cout << "算法运行时间：" << run_time << "毫秒" << endl;
+	}
+	totaltime = clock();
+	cout << "程序运行时间：" << totaltime / 1000 << "秒" << endl;
+	if (run_time > 0.4)
+	{
+		double frequency = t3 / run_time / 1000;
+		cout << "CPU频率：" << frequency << "MHZ" << endl;
+	}
+	ShowProcessMemoryUsageInfo();
+	long lRet;
+	HKEY hKey;
+	TCHAR tchData[64];
+	DWORD dwSize;
+	lRet = RegOpenKeyEx(HKEY_LOCAL_MACHINE, L"Hardware\\Description\\System\\CentralProcessor\\0", 0, KEY_QUERY_VALUE, &hKey);
+	if (lRet == ERROR_SUCCESS)
+	{
+		dwSize = sizeof(tchData);
+		lRet = RegQueryValueEx(hKey, L"ProcessorNameString", NULL, NULL, (LPBYTE)tchData, &dwSize);
+		if (lRet == ERROR_SUCCESS)
+		{
+			cout << "CPU 名字：";
+			wcout << tchData << endl;
+		}
+		else
+		{
+			cout << "CPU 名字：";
+			wcout << L"未知" << endl;
+		}
+	}
+	else
+	{
+		cout << "CPU 名字：";
+		wcout << L"未知" << endl;
+	}
+	RegCloseKey(hKey);
+	MEMORYSTATUSEX statex;
+	statex.dwLength = sizeof(statex);
+	GlobalMemoryStatusEx(&statex);
+	double totalphy = statex.ullTotalPhys / 1024 / 1024;
+	totalphy = totalphy / 1024;
+	cout << "总物理内存：    " << setprecision(4) << left << setw(7) << totalphy << " GB" << endl;
+	double totalvir = statex.ullTotalPageFile / 1024 / 1024;
+	totalvir = totalvir / 1024;
+	cout << "总虚拟内存：    " << setw(7) << totalvir - totalphy << " GB" << endl;
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD coordScreen = { 0, 0 }; //光标位置
+	CONSOLE_SCREEN_BUFFER_INFO csbi{};
+	if (GetConsoleScreenBufferInfo(hConsole, &csbi))
+	{
+		//printf("光标坐标:(%d,%d)\n", csbi.dwCursorPosition.X, csbi.dwCursorPosition.Y);
+	}
+	HANDLE   hStdout;
+	//   光标位置   
+	COORD     cursorPos;
+	//   标准输出句柄   
+	hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+	HANDLE hEvent;
+	BOOL res;
+	FILETIME preidleTime;
+	FILETIME prekernelTime;
+	FILETIME preuserTime;
+	FILETIME idleTime;
+	FILETIME kernelTime;
+	FILETIME userTime;
+	res = GetSystemTimes(&idleTime, &kernelTime, &userTime);
+	preidleTime = idleTime;
+	prekernelTime = kernelTime;
+	preuserTime = userTime;
+	hEvent = CreateEvent(NULL, FALSE, FALSE, NULL);// 初始值为 nonsignaled ，并且每次触发后自动设置为nonsignaled
+	double freephy = statex.ullAvailPhys / 1024 / 1024;
+	freephy = freephy / 1024;
+	double usephy = (totalphy - freephy) * 100.0 / totalphy;
+	cout << "内存使用率：    " << setprecision(3) << usephy << "%" << endl;
+	double freetotal = statex.ullAvailPageFile / 1024 / 1024;
+	freetotal = freetotal / 1024;
+	cout << "总空闲内存：    " << setprecision(3) << freetotal << " GB" << endl;
+	double freevir = (totalvir - freetotal) - (totalphy * statex.dwMemoryLoad / 100.0);
+	cout << "虚拟内存使用量：" << setprecision(3) << freevir << " GB" << endl;
+	double usevir = freevir * 100 / (totalvir - totalphy);
+	cout << "虚拟内存使用率：";
+	printf("%.2f%%", usevir);
+	cout << endl;
+	cout << "CPU利用率：    " << right << setprecision(2) << setw(6) << 0.00 << "%" << endl;
+	cout << "CPU空闲率：    " << setw(6) << 100.0<< "%" << endl;
+	cout << "-------------------------------------------------------" << endl;
+	cout << "按任意键退出程序" << endl;
+	while (!_kbhit())
+	{
+		GlobalMemoryStatusEx(&statex);
+		WaitForSingleObject(hEvent, 1000);
+		res = GetSystemTimes(&idleTime, &kernelTime, &userTime);
+		int idle = CompareFileTime(preidleTime, idleTime);
+		int kernel = CompareFileTime(prekernelTime, kernelTime);
+		int user = CompareFileTime(preuserTime, userTime);
+		float cpu = (kernel + user - idle) * 100.0 / (kernel + user);
+		float cpuidle = (idle) * 100.0 / (kernel + user);
+		cursorPos.X = 0;
+		cursorPos.Y = csbi.dwCursorPosition.Y + 0;
+		SetConsoleCursorPosition(hStdout, cursorPos);
+		//cout << "内存使用率：" << statex.dwMemoryLoad << "%" << endl;
+		double freephy = statex.ullAvailPhys / 1024 / 1024;
+		freephy = freephy / 1024;
+		double usephy = (totalphy - freephy) * 100.0 / totalphy;
+		cout << "内存使用率：    " << setprecision(3) << usephy << "%" << endl;
+		cursorPos.X = 0;
+		cursorPos.Y = csbi.dwCursorPosition.Y + 1;
+		SetConsoleCursorPosition(hStdout, cursorPos);
+		double freetotal = statex.ullAvailPageFile / 1024 / 1024;
+		freetotal = freetotal / 1024;
+		cout << "总空闲内存：    " << setprecision(3) << freetotal <<" GB" << endl;
+		cursorPos.X = 0;
+		cursorPos.Y = csbi.dwCursorPosition.Y + 2;
+		SetConsoleCursorPosition(hStdout, cursorPos);
+		double freevir = (totalvir - freetotal) - (totalphy * statex.dwMemoryLoad / 100.0);
+		cout << "虚拟内存使用量：" << setprecision(3) << freevir <<" GB" << endl;
+		cursorPos.X = 0;
+		cursorPos.Y = csbi.dwCursorPosition.Y + 3;
+		SetConsoleCursorPosition(hStdout, cursorPos);
+		double usevir = freevir * 100 / (totalvir - totalphy);
+		cout << "虚拟内存使用率：";
+		printf("%.2f%%", usevir);
+		cout << endl;
+		cursorPos.X = 0;
+		cursorPos.Y = csbi.dwCursorPosition.Y + 4;
+		SetConsoleCursorPosition(hStdout, cursorPos);
+		cout << "CPU利用率：    " << right << setprecision(2) << setw(6) << cpu << "%" << endl;
+		cursorPos.X = 0;
+		cursorPos.Y = csbi.dwCursorPosition.Y + 5;
+		SetConsoleCursorPosition(hStdout, cursorPos);
+		cout << "CPU空闲率：    " << setw(6) << cpuidle << "%"<<endl;
+		cursorPos.X = 0;
+		cursorPos.Y = csbi.dwCursorPosition.Y + 6;
+		SetConsoleCursorPosition(hStdout, cursorPos);
+		cout << "-------------------------------------------------------" << endl;
+		cursorPos.X = 0;
+		cursorPos.Y = csbi.dwCursorPosition.Y + 7;
+		SetConsoleCursorPosition(hStdout, cursorPos);
+		cout << "按任意键退出程序" << endl;
+		preidleTime = idleTime;
+		prekernelTime = kernelTime;
+		preuserTime = userTime;
+	}
+	cout << endl;
+	
+}
+
+
+//**************************************************************//************************非主函数
+
+#include <windows.h>
+#include <tlhelp32.h>
+#include <tchar.h>
+
+//  Forward declarations:
+BOOL GetProcessList();
+BOOL ListProcessModules(DWORD dwPID);
+BOOL ListProcessThreads(DWORD dwOwnerPID);
+void printError(TCHAR* msg);
+
+BOOL GetProcessList()
+{
+	HANDLE hProcessSnap;
+	HANDLE hProcess;
+	PROCESSENTRY32 pe32;
+	DWORD dwPriorityClass;
+
+	// Take a snapshot of all processes in the system.
+	hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (hProcessSnap == INVALID_HANDLE_VALUE)
+	{
+		printError(TEXT("CreateToolhelp32Snapshot (of processes)"));
+		return(FALSE);
+	}
+
+	// Set the size of the structure before using it.
+	pe32.dwSize = sizeof(PROCESSENTRY32);
+
+	// Retrieve information about the first process,
+	// and exit if unsuccessful
+	if (!Process32First(hProcessSnap, &pe32))
+	{
+		printError(TEXT("Process32First")); // show cause of failure
+		CloseHandle(hProcessSnap);          // clean the snapshot object
+		return(FALSE);
+	}
+
+	// Now walk the snapshot of processes, and
+	// display information about each process in turn
+	do
+	{
+		_tprintf(TEXT("\n\n====================================================="));
+		_tprintf(TEXT("\nPROCESS NAME:  %s"), pe32.szExeFile);
+		_tprintf(TEXT("\n-------------------------------------------------------"));
+
+		// Retrieve the priority class.
+		dwPriorityClass = 0;
+		hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pe32.th32ProcessID);
+		if (hProcess == NULL)
+			printError(TEXT("OpenProcess"));
+		else
+		{
+			dwPriorityClass = GetPriorityClass(hProcess);
+			if (!dwPriorityClass)
+				printError(TEXT("GetPriorityClass"));
+			CloseHandle(hProcess);
+		}
+
+		_tprintf(TEXT("\n  Process ID        = 0x%08X"), pe32.th32ProcessID);
+		_tprintf(TEXT("\n  Thread count      = %d"), pe32.cntThreads);
+		_tprintf(TEXT("\n  Parent process ID = 0x%08X"), pe32.th32ParentProcessID);
+		_tprintf(TEXT("\n  Priority base     = %d"), pe32.pcPriClassBase);
+		if (dwPriorityClass)
+			_tprintf(TEXT("\n  Priority class    = %d"), dwPriorityClass);
+
+		// List the modules and threads associated with this process
+		ListProcessModules(pe32.th32ProcessID);
+		ListProcessThreads(pe32.th32ProcessID);
+
+	} while (Process32Next(hProcessSnap, &pe32));
+
+	CloseHandle(hProcessSnap);
+	return(TRUE);
+}
+
+
+BOOL ListProcessModules(DWORD dwPID)
+{
+	HANDLE hModuleSnap = INVALID_HANDLE_VALUE;
+	MODULEENTRY32 me32;
+
+	// Take a snapshot of all modules in the specified process.
+	hModuleSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, dwPID);
+	if (hModuleSnap == INVALID_HANDLE_VALUE)
+	{
+		printError(TEXT("CreateToolhelp32Snapshot (of modules)"));
+		return(FALSE);
+	}
+
+	// Set the size of the structure before using it.
+	me32.dwSize = sizeof(MODULEENTRY32);
+
+	// Retrieve information about the first module,
+	// and exit if unsuccessful
+	if (!Module32First(hModuleSnap, &me32))
+	{
+		printError(TEXT("Module32First"));  // show cause of failure
+		CloseHandle(hModuleSnap);           // clean the snapshot object
+		return(FALSE);
+	}
+
+	// Now walk the module list of the process,
+	// and display information about each module
+	do
+	{
+		_tprintf(TEXT("\n\n     MODULE NAME:     %s"), me32.szModule);
+		_tprintf(TEXT("\n     Executable     = %s"), me32.szExePath);
+		_tprintf(TEXT("\n     Process ID     = 0x%08X"), me32.th32ProcessID);
+		_tprintf(TEXT("\n     Ref count (g)  = 0x%04X"), me32.GlblcntUsage);
+		_tprintf(TEXT("\n     Ref count (p)  = 0x%04X"), me32.ProccntUsage);
+		_tprintf(TEXT("\n     Base address   = 0x%08X"), (DWORD)me32.modBaseAddr);
+		_tprintf(TEXT("\n     Base size      = %d"), me32.modBaseSize);
+
+	} while (Module32Next(hModuleSnap, &me32));
+
+	CloseHandle(hModuleSnap);
+	return(TRUE);
+}
+
+BOOL ListProcessThreads(DWORD dwOwnerPID)
+{
+	HANDLE hThreadSnap = INVALID_HANDLE_VALUE;
+	THREADENTRY32 te32;
+
+	// Take a snapshot of all running threads  
+	hThreadSnap = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+	if (hThreadSnap == INVALID_HANDLE_VALUE)
+		return(FALSE);
+
+	// Fill in the size of the structure before using it. 
+	te32.dwSize = sizeof(THREADENTRY32);
+
+	// Retrieve information about the first thread,
+	// and exit if unsuccessful
+	if (!Thread32First(hThreadSnap, &te32))
+	{
+		printError(TEXT("Thread32First")); // show cause of failure
+		CloseHandle(hThreadSnap);          // clean the snapshot object
+		return(FALSE);
+	}
+
+	// Now walk the thread list of the system,
+	// and display information about each thread
+	// associated with the specified process
+	do
+	{
+		if (te32.th32OwnerProcessID == dwOwnerPID)
+		{
+			_tprintf(TEXT("\n\n     THREAD ID      = 0x%08X"), te32.th32ThreadID);
+			_tprintf(TEXT("\n     Base priority  = %d"), te32.tpBasePri);
+			_tprintf(TEXT("\n     Delta priority = %d"), te32.tpDeltaPri);
+			_tprintf(TEXT("\n"));
+		}
+	} while (Thread32Next(hThreadSnap, &te32));
+
+	CloseHandle(hThreadSnap);
+	return(TRUE);
+}
+
+void printError(TCHAR* msg)
+{
+	DWORD eNum;
+	TCHAR sysMsg[256];
+	TCHAR* p;
+
+	eNum = GetLastError();
+	FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL, eNum,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), // Default language
+		sysMsg, 256, NULL);
+
+	// Trim the end of the line and terminate it with a null
+	p = sysMsg;
+	while ((*p > 31) || (*p == 9))
+		++p;
+	do { *p-- = 0; } while ((p >= sysMsg) &&
+		((*p == '.') || (*p < 33)));
+
+	// Display the message
+	_tprintf(TEXT("\n  WARNING: %s failed with error %d (%s)"), msg, eNum, sysMsg);
+}
+
+//**************************************************************//************************
+
+int main()
+{
+
+	//*************************************************************************
+
+
+
+	//*************************************************************************
+	runstart();
+	//*************************计时开始***************************************
+	
+	GetProcessList();
+
+	//*************************计时结束***************************************
+	runend();
+
+
+
+	_getch();
+
+
+
+	rundisplay();                                                                   //计时结果显示
+	_getch();
+	//closegraph();
+	// system("pause");
+	//Sleep(10000);
+	return 0;
+}
